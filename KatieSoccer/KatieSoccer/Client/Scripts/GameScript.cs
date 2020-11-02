52e0d14772d3c0f3ce5876fe3163c254ae1b0dc4
Spencer Stream <sprewstream@gmail.com>
2020-11-02 15:47:46 -0600

    Adding the Update loop and experimenting with piece movement

--- KatieSoccer/KatieSoccer/Client/Scripts/GameScript.cs
+++ KatieSoccer/KatieSoccer/Client/Scripts/GameScript.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using KatieSoccer.Client.Models;
 
 namespace KatieSoccer.Client.Scripts
 {
     public class GameScript
     {
+        public event EventHandler OnUpdate;
+
         public List<PieceModel> TeamOnePieces { get; set; }
         public List<PieceModel> TeamTwoPieces { get; set; }
 
@@ -47,6 +51,30 @@ namespace KatieSoccer.Client.Scripts
                     Color = "#2499F2"
                 }
             };
+        }
+
+        private float speed = 15;
+
+        public async Task Update()
+        {
+            while (true)
+            {
+                TeamOnePieces[0].Position = new Position(
+                    TeamOnePieces[0].Position.X + speed,
+                    TeamOnePieces[0].Position.Y + speed);
+
+                if (speed > 0)
+                {
+                    speed -= 0.1f;
+                }
+                else
+                {
+                    speed = 0;
+                }
+
+                OnUpdate?.Invoke(this, EventArgs.Empty);
+                await Task.Delay(10);
+            }
         }
     }
 }
