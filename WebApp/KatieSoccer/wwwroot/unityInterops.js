2560e6f550d16eaadc5907782b3f196cf8eb8fb8
Spencer Stream <sprewstream@gmail.com>
2020-12-05 15:21:42 -0600

    Abstracting game and related JS code into their own files

---
+++ WebApp/KatieSoccer/wwwroot/unityInterops.js
@@ -0,0 +1,10 @@
+﻿var unityInstance;
+
+window.instantiateGame = () => {
+	unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebGL.json", { onProgress: UnityProgress });
+}
+
+window.initializeGame = () => {
+	unityInstance.SendMessage("Field", "SetTeamNames", '{"TeamOneName": "TeamOne", "TeamTwoName": "TeamTwo"}');
+	unityInstance.SendMessage("Field", "SetTeamColors", '{"TeamOneColor": {"R": 1.0, "G": 0.69, "B": 0.56}, "TeamTwoColor": {"R": 0.65, "G": 0.65, "B": 0.93}}');
+}
