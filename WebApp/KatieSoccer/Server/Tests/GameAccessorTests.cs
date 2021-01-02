bd26ad0f37c5d84bd5617883f91b00b838cf4338
Spencer Stream <sprewstream@gmail.com>
2021-01-02 00:28:13 -0600

    Adding test for GameAccessor GetGames()

--- WebApp/KatieSoccer/Server/Tests/GameAccessorTests.cs
+++ WebApp/KatieSoccer/Server/Tests/GameAccessorTests.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using AutoMapper;
 using KatieSoccer.Server.Accessors.EntityFramework;
@@ -15,6 +16,44 @@ namespace KatieSoccer.Server.Accessors.Tests
     {
         private List<Game> EmptyGames => new List<Game>();
 
+        private List<Game> Games => new List<Game>
+        {
+            new Game
+            {
+                GameId = "testgameId",
+                IsOnline = false,
+                PlayerOne = new EntityFramework.Models.Player
+                {
+                    Color = "red",
+                    IsLocal = true,
+                    Name = "player1"
+                },
+                PlayerTwo = new EntityFramework.Models.Player
+                {
+                    Color = "blue",
+                    IsLocal = true,
+                    Name = "player2"
+                }
+            },
+            new Game
+            {
+                GameId = "testgameId2",
+                IsOnline = true,
+                PlayerOne = new EntityFramework.Models.Player
+                {
+                    Color = "red",
+                    IsLocal = false,
+                    Name = "player3"
+                },
+                PlayerTwo = new EntityFramework.Models.Player
+                {
+                    Color = "blue",
+                    IsLocal = true,
+                    Name = "player4"
+                }
+            }
+        };
+
         private Game TestGame => new Game
         {
             GameId = "testgameId",
@@ -29,6 +68,44 @@ namespace KatieSoccer.Server.Accessors.Tests
                 Color = "blue",
                 IsLocal = false,
                 Name = "player2"
+            }
+        };
+
+        private List<GameData> TestGameDatas => new List<GameData>
+        {
+            new GameData
+            {
+                GameId = "testgameId",
+                IsOnline = false,
+                PlayerOne = new Shared.Player
+                {
+                    Color = "red",
+                    IsLocal = true,
+                    Name = "player1"
+                },
+                PlayerTwo = new Shared.Player
+                {
+                    Color = "blue",
+                    IsLocal = true,
+                    Name = "player2"
+                }
+            },
+            new GameData
+            {
+                GameId = "testgameId2",
+                IsOnline = true,
+                PlayerOne = new Shared.Player
+                {
+                    Color = "red",
+                    IsLocal = false,
+                    Name = "player3"
+                },
+                PlayerTwo = new Shared.Player
+                {
+                    Color = "blue",
+                    IsLocal = true,
+                    Name = "player4"
+                }
             }
         };
 
@@ -66,6 +143,30 @@ namespace KatieSoccer.Server.Accessors.Tests
             };
 
             await gameAccessor.AddGame(gameToAdd);
+        }
+
+        [Fact]
+        public async Task GetGames_ReturnsGameDatas()
+        {
+            var mockMapper = new Mock<IMapper>();
+            mockMapper
+                .Setup(m => m.Map<List<GameData>>(It.IsAny<List<Game>>()))
+                .Returns(TestGameDatas);
+
+            var mockDbSet = Games.AsQueryable().BuildMockDbSet();
+            var mockDbContext = new Mock<IKatieSoccerDbContext>();
+            mockDbContext
+                .Setup(db => db.Games)
+                .Returns(mockDbSet.Object);
+
+            var gameAccessor = new GameAccessor(mockMapper.Object, mockDbContext.Object);
+
+            var actualGameDatas = await gameAccessor.GetGames();
+
+            // Serializing as JSON so the objects can be directly compared
+            var serializedGameDatas = JsonSerializer.Serialize(actualGameDatas);
+            var expectedSerializedGameDatas = JsonSerializer.Serialize(TestGameDatas);
+            Assert.Equal(expectedSerializedGameDatas, serializedGameDatas);
         }
     }
 }
