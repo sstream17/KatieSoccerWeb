2ab11cbb00e633e4a57cbfdfcb0ba1f268ffbb8b
Spencer Stream <sprewstream@gmail.com>
2021-01-09 16:31:52 -0600

    Adding a GameController to the API to handle creating games

---
+++ WebApp/KatieSoccer/Server/Clients/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using KatieSoccer.Server.Accessors;
+using KatieSoccer.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KatieSoccer.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController : ControllerBase
+    {
+        public GameController(IGameAccessor gameAccessor)
+        {
+            GameAccessor = gameAccessor;
+        }
+
+        private IGameAccessor GameAccessor { get; }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGame(GameData gameData)
+        {
+            await GameAccessor.AddGame(gameData);
+            return Created("/play", null);
+        }
+    }
+}
