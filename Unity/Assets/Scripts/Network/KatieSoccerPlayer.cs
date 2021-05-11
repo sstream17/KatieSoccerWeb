417994cd05f21d073f600a6d6acc105c2fc85c2b
Spencer Stream <sprewstream@gmail.com>
2021-05-11 17:24:30 -0500

    Starting scripts for the NetworkManager and Player

---
+++ Unity/Assets/Scripts/Network/KatieSoccerPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Mirror;
+using System.Collections.Generic;
+
+/*
+	Documentation: https://mirror-networking.com/docs/Guides/NetworkBehaviour.html
+	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkBehaviour.html
+*/
+
+// NOTE: Do not put objects in DontDestroyOnLoad (DDOL) in Awake.  You can do that in Start instead.
+
+public class KatieSoccerPlayer : NetworkBehaviour
+{
+	public Rigidbody[] PieceRigidbodies;
+
+	[Command]
+	public void CmdPlayTurn(int pieceIndex, Vector3 force)
+    {
+		var rb = PieceRigidbodies[pieceIndex];
+
+		if (rb != null)
+        {
+			rb.AddForce(force);
+        }
+    }
+}
